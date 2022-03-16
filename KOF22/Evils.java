98cc27288b1ec77e522fa0fb1c8f44cb4a3e4c5c
Sespeck <jacfan17@gmail.com>
2022-03-17 04:36:29 +0800

    sean

--- KOF22/Evils.java
+++ KOF22/Evils.java
@@ -1,29 +1,20 @@
 public class Evils extends CharaterList{
     private ArrayList<Character> evilList;
     public Evils(){
-        evilList = new ArrayList<evil>();
-     
-     /*  Constructor of a character(change to hero and evil respectively)
-        public Character(String name, int hp, int power){
-        this.alive = true;
-        this.name = name;
-        this.hitPoints = hp;
-        this.power = power;
-        }
-        */
-      	
+       ArrayList<Evil> evilList = new ArrayList<Evil>();
+    
     	evilList.add(new BadGuy("zombie", 200, 10));
     	evilList.add(new BadGuy("werewolf", 200, 10));
     	evilList.add(new BadGuy("goblin", 200, 10));
     }
 
      public void out(int index){
-        // Out the evil from the evilList
+        evilList.remove(index);
     }
 
     public String print(){
         for(int x = 0; x<evilList.getSize(); x++){
-			System.out.print(" " + evilList.get(x) + " ");
+			System.out.print(evilList.get(x) + " ");
 		}
     }
 }
--- KOF22/Heroes.java
+++ KOF22/Heroes.java
@@ -1,17 +1,11 @@
 public class Heroes extends CharaterList{
     private ArrayList<Character> heroList;
     public Heroes(){
-        heroList = new ArrayList<hero>();
-     
-     /*
-     public Hero(String name, int hp, int power, int mana){
-        super(name, hp, power);
-        this.mana = mana;
-    }
-    */
-        heroList.add(new Hero("sakura", 100, 20, 10, 5));   	
-    	heroList.add(new Hero("chunli", 100, 25, 10, 5));
-    	heroList.add(new Hero("ken", 120, 20, 10, 5));
+        ArrayList<Hero> heroList = new ArrayList<Hero>();
+
+        heroList.add(new Hero("sakura", 100, 20, 10, 15));   	
+    	heroList.add(new Hero("chunli", 100, 25, 10, 15));
+    	heroList.add(new Hero("ken", 120, 20, 10, 15));
     }
 
     public void out(int index){
@@ -20,7 +14,7 @@ public class Heroes extends CharaterList{
 
     public String print(){
         for(int x = 0; x<heroList.getSize(); x++){
-			System.out.print(" " + heroList.get(x).getName() + " ");
+			System.out.print(heroList.get(x).getName() + " ");
 		}
     }
 }
